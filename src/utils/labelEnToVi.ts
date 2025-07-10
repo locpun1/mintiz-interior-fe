e094594c4da275b4aa0f4e4c4e6f460002c2b8bb
anhpn <ngocanh.pham19042001@gmail.com>
2025-07-10 17:03:43 +0700

    get api posts for landing page

--- src/utils/labelEnToVi.ts
+++ src/utils/labelEnToVi.ts
@@ -4,3 +4,11 @@ export const getRoleLabel = (role: RoleUser | null | undefined): string => {
     if(!role) return "Chưa xác định";
     return ROLE_LABELS[role] || role
 }
+
+export const getFormatText = (content: string): string => {
+    const formatText = content
+        .replace(/<p>/g, '')         // bỏ <p>
+        .replace(/<\/p>/g, '\n\n')    // đổi </p> thành xuống 2 dòng
+        .replace(/<[^>]+>/g, '');     // remove tất cả tag html còn lại
+    return formatText;
+}
--- src/views/LandingPage/News/components/ContentNewDetail.tsx
+++ src/views/LandingPage/News/components/ContentNewDetail.tsx
@@ -1,18 +1,49 @@
 import { Box, Button, Stack, Typography } from "@mui/material";
 import React from "react";
-import { getCategoryLabel, NewsListProps } from "..";
+import { getCategoryLabel } from "..";
 import CommonImage from "@/components/Image/index";
-import { CONTENT_NEWS } from "@/constants/contentAbout";
 import { Facebook, Instagram, KeyboardBackspace, LinkedIn, Twitter } from "@mui/icons-material";
 import IconButton from "@/components/IconButton/IconButton";
+import { IPost } from "@/types/post";
+import dayjs from "dayjs";
+import { getPathImage } from "@/utils/url";
+import parse from 'html-react-parser';
 
 interface ContentNewDetailProps{
-    newContent: NewsListProps,
+    newContent: IPost,
     handleToggle: () => void;
 }
 
+interface NewsDetailProps {
+    content: string;
+}
+
+const NewsDetail: React.FC<NewsDetailProps> = ({ content }) => {
+  return (
+    <Box
+      sx={{
+        '& img': {
+          display: 'block',
+          margin: '16px auto', // căn giữa ngang
+          maxWidth: '100%',
+          height: 'auto',
+        },
+        '& p': {
+          textAlign: 'justify', // tuỳ bạn
+          marginBottom: '12px',
+        }
+      }}
+    >
+        <Typography sx={{ whiteSpace: 'normal', wordBreak: 'break-word', mt:1, fontSize: {xs: '14px', md: '16px'} }}>    
+            {parse(content)}
+        </Typography>
+    </Box>
+  );
+};
+
 const ContentNewDetail: React.FC<ContentNewDetailProps> = (props) => {
     const { newContent, handleToggle } = props;
+    const date = dayjs(newContent.updatedAt).format('MMM DD,YYYY')
     return(
         <Box>
             <Box
@@ -26,11 +57,11 @@ const ContentNewDetail: React.FC<ContentNewDetailProps> = (props) => {
                     flexDirection='column'
             >
                 <Box sx={{ mb: 2}} display='flex' justifyContent='space-evenly'>
-                    <Typography variant="caption" sx={{ opacity: 0.7 }}>
+                    <Typography variant="subtitle2" sx={{ opacity: 0.7 }}>
                         {getCategoryLabel(newContent.category)}
                     </Typography>
-                    <Typography variant="caption" sx={{ opacity: 0.7 }}>
-                        {newContent.date}
+                    <Typography variant="subtitle2" sx={{ opacity: 0.7 }}>
+                        {date}
                     </Typography>
                 </Box>
                 <Typography
@@ -43,20 +74,18 @@ const ContentNewDetail: React.FC<ContentNewDetailProps> = (props) => {
                 </Typography>
                 <Typography sx={{ fontSize: { xs: '13px', md: '16px'}}} textAlign='center' mb={5}>{`Trang chủ/Tin tức/${newContent.title}`}</Typography>
                 <CommonImage
-                    src={newContent.image}
+                    src={getPathImage(newContent.imageUrl)}
                     sx={{
                         height: { xs: '100%', md: 600},
                         mb: 5
                     }}
                 />
-                {CONTENT_NEWS.map((param, idx) => (
-                    <Typography key={idx} sx={{ whiteSpace: 'normal', wordBreak: 'break-word', mt:1, fontSize: {xs: '14px', md: '16px'} }}>{param}</Typography>
-                ))}
+                <NewsDetail content={newContent.content}/>
                 <Button
                     onClick={handleToggle}
                     variant="text"
                     sx={{ 
-                        mt: 1, color: "white", fontWeight: 500, fontSize: '16px', textDecoration: 'underline', 
+                        mt: 1, color: "white", fontWeight: 500, fontSize: '18px', textDecoration: 'underline', 
                         '&:hover': { textDecoration: 'underline', textDecorationColor: '#fff', bgcolor: '#1C1A1B'} 
                     }}
                     startIcon={<KeyboardBackspace/>}
